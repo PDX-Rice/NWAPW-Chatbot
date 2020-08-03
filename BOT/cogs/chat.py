8f22e919d4e46b078232f97e192db61ac7aa32dd
Jorge Raposo <jorgealberto.p.raposo@hotmail.com>
2020-08-03 13:14:24 -0700

    Update chat.py

--- BOT/cogs/chat.py
+++ BOT/cogs/chat.py
@@ -8,17 +8,12 @@ class Chat(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
+    @commands.command(aliases=["bruh"])
+    async def hello(self, ctx):
         greetings = ["hey!", "hi!",
                     "hello!", "yo!",
                     "wassup!", ]
-
-        if message.author == self.client.user:
-            return
-
-        if message.content.startswith('hello'):
-            await message.channel.send(random.choice(greetings).title())
+        await ctx.send(random.choice(greetings).title())
 
 def setup(client):
     client.add_cog(Chat(client))
