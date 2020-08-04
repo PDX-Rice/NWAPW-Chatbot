a182c718a0985a113ef5ef114cd43f4012c6dcf4
Jorge Raposo <jorgealberto.p.raposo@hotmail.com>
2020-08-04 11:27:36 -0700

    Update chatbotpython.py

--- BOT/chatbotpython.py
+++ BOT/chatbotpython.py
@@ -44,12 +44,6 @@ async def reload(ctx, extension):
 
 
 @client.command()
-async def prefix(ctx, pre):
-    #client = commands.Bot(command_prefix = pre)
-    await ctx.send('Successfully changed the prefix to " ' + pre + ' " (not really though )') #doesn't change the prefix yet, just the message for now
-
-
-@client.command()
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 
@@ -89,4 +83,4 @@ for filename in os.listdir('./cogs'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
 
-client.run('INSERT TOKEN HERE')
+client.run(TOKEN)
